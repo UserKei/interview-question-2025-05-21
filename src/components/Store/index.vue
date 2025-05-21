7ccf1a867eb15bc412d212118011fa4d755b2ded
User_Kei <user_kei@outlook.com>
2025-05-22 01:13:30 +0800

    solve formating code don't work

--- src/components/Store/index.vue
+++ src/components/Store/index.vue
@@ -1,6 +1,15 @@
 <script setup>
+import { ref } from 'vue'
 import stroe from '@/assets/store.png'
 import location from '@/assets/location.png'
+
+const storeStatistics = ref([
+  { label: '已选门店', value: '23' },
+  { label: '活跃会员数', value: '232,300' },
+  { label: '平均月销售额', value: '¥222,423' },
+  { label: '平均售罄率', value: '45%' },
+  { label: '客单价', value: '¥34,544' },
+])
 </script>
 
 <template>
@@ -8,6 +17,7 @@ import location from '@/assets/location.png'
     <template #header>
       <span>圈选店人数</span>
     </template>
+
     <div>
       <img :src="stroe" alt="" />
       <span>店</span>
@@ -16,5 +26,13 @@ import location from '@/assets/location.png'
       <span>主要区域：广州</span>
     </div>
 
+    <div>
+      <div class="store-stats">
+        <div class="stat-item" v-for="(item, index) in storeStatistics" :key="index">
+          <div class="label">{{ item.label }}</div>
+          <div class="value">{{ item.value }}</div>
+        </div>
+      </div>
+    </div>
   </el-card>
 </template>
