e46ecdc593f7548eb63f90a990aa67049c3c1c9d
User_Kei <user_kei@outlook.com>
2025-05-22 00:56:18 +0800

    update Store components

--- src/components/Store/index.vue
+++ src/components/Store/index.vue
@@ -1,1 +1,20 @@
-<script></script>
+<script setup>
+import stroe from '@/assets/store.png'
+import location from '@/assets/location.png'
+</script>
+
+<template>
+  <el-card>
+    <template #header>
+      <span>圈选店人数</span>
+    </template>
+    <div>
+      <img :src="stroe" alt="" />
+      <span>店</span>
+
+      <img :src="location" alt="" />
+      <span>主要区域：广州</span>
+    </div>
+
+  </el-card>
+</template>
